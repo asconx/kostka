66644a6b2806832fd5ce1bea11cab38b56e4cc16
DESKTOP-L44RQMV\JakubBurandt4TP <kuba.burandt@gmail.com>
2024-11-18 09:39:19 +0100

    csharpjb

---
+++ kostka/Kostka/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Kostka
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
